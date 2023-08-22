5f8d143a647a83349b12e7a91bd3db09da59e053
Yamin-Sidheeque <141262879+Yamin-Sidheeque@users.noreply.github.com>
2023-08-22 14:17:34 +0530

    Delete Pizza Sales SQLQuery.sql

--- Pizza Sales SQLQuery.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM pizza_sales;
- --Daily Trend
- SELECT DATENAME (DW, order_date) AS Order_Day, COUNT(DISTINCT order_id) AS Total_Orders
- FROM pizza_sales
- GROUP BY DATENAME(DW, order_date);
